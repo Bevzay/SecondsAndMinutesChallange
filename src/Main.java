5243b725cddeea69445c2cc1cf2f9332774290a5
Beverly Ann Gupit <Bevzay@users.noreply.github.com>
2020-06-23 15:32:12 +1200

    2nd method worked

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
 
         SecondsAndMinutesChallenge.getDurationString(90, 12);
+        SecondsAndMinutesChallenge.getDurationString(4500);
 
     }
 }
--- src/SecondsAndMinutesChallenge.java
+++ src/SecondsAndMinutesChallenge.java
@@ -32,6 +32,7 @@ public class SecondsAndMinutesChallenge {
 
     public static int getDurationString(int minutes, int seconds) {
         if((minutes < 0) || (seconds < 0) || (seconds > 59)) {
+            System.out.println("Invalid Value");
             return -1;
         } else {
             int hours = minutes / 60;
@@ -40,6 +41,18 @@ public class SecondsAndMinutesChallenge {
             return remainingMinutes;
         }
 
+    }
+
+    public static int getDurationString(int seconds) {
+        if(seconds < 0) {
+            System.out.println("Invalid Value");
+            return -1;
+        } else {
+            int minutes = seconds / 60;
+            int remainingMinutes = seconds % 60;
+            System.out.println(seconds + "s = " + minutes + "m " + remainingMinutes + "s");
+            return getDurationString(minutes, remainingMinutes);
+        }
     }
 
 }
