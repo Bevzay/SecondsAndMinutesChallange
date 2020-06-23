5cf49d878d952b66b29ba48e18419265b0f2c351
Beverly Ann Gupit <Bevzay@users.noreply.github.com>
2020-06-23 15:15:57 +1200

    Add challenge

--- src/SecondsAndMinutesChallenge.java
+++ src/SecondsAndMinutesChallenge.java
@@ -1,3 +1,37 @@
 public class SecondsAndMinutesChallenge {
 
+    /*
+     * Create a method called getDurationString
+     * with 2 parameters, minutes and seconds
+     *
+     * You should validate that 1st parameter minutes is >=0
+     * You should validate that 2nd parameter seconds is >= 0 and <= 59
+     *
+     * The method should return "Invalid value" in the method
+     * if either the above are not true
+     *
+     * If the parameters are valid then calculate how many hours,
+     * minutes and seconds equals the minutes and seconds passed
+     * on this method and return that value as string format "XXh YYm ZZs"
+     *
+     * Create a 2nd method of the same name
+     * but with one parameter, seconds
+     *
+     * Validate that it is >= 0,
+     * return "Invalid value" if its not true
+     *
+     * If it is valid, then calculate how many minutes are in
+     * the seconds value and then call the other overloaded method
+     * passing the correct minutes and seconds calculated
+     * so that it can calculate correctly
+     *
+     * Call both methods to print values to the console
+     *
+     * Tips: Use int, 1 min = 60 s, 1 hr = 60 minutes = 3600 s
+     */
+
+    public static int getDurationString(int minutes, int seconds) {
+
+    }
+
 }
